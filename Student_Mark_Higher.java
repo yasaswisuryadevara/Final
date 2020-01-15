82eb52fba16d6ceff0eb00b362a4c5faedeec279
yasaswisuryadevara <59167248+yasaswisuryadevara@users.noreply.github.com>
2020-01-14 22:27:02 -0800

    Add files via upload

---
+++ Student_Mark_Higher.java
@@ -0,0 +1,63 @@
+package org.deloitte.sms.studentstaffdetailsmanagement.beans;
+
+public class Student_Mark_Higher {
+	 private String student_Id;
+     private int english_Mark;
+     private int hindi_Mark;
+     private int science_Mark;
+     private int math_Mark;
+     private int total_Mark;
+     private int average_Mark;
+     private String grade;
+	public String getStudent_Id() {
+		return student_Id;
+	}
+	public void setStudent_Id(String student_Id) {
+		this.student_Id = student_Id;
+	}
+	public int getEnglish_Mark() {
+		return english_Mark;
+	}
+	public void setEnglish_Mark(int english_Mark) {
+		this.english_Mark = english_Mark;
+	}
+	public int getHindi_Mark() {
+		return hindi_Mark;
+	}
+	public void setHindi_Mark(int hindi_Mark) {
+		this.hindi_Mark = hindi_Mark;
+	}
+	public int getScience_Mark() {
+		return science_Mark;
+	}
+	public void setScience_Mark(int science_Mark) {
+		this.science_Mark = science_Mark;
+	}
+	public int getMath_Mark() {
+		return math_Mark;
+	}
+	public void setMath_Mark(int math_Mark) {
+		this.math_Mark = math_Mark;
+	}
+	public int getTotal_Mark() {
+		return total_Mark;
+	}
+	public void setTotal_Mark(int total_Mark) {
+		this.total_Mark = total_Mark;
+	}
+	public int getAverage_Mark() {
+		return average_Mark;
+	}
+	public void setAverage_Mark(int average_Mark) {
+		this.average_Mark = average_Mark;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+     
+     
+
+}
